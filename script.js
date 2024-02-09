51f9c83767a228ac9825e441e4c214d3db756393
Anand Thombre <153847243+thombreanand@users.noreply.github.com>
2024-02-09 22:52:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll('.about-btn button');
+const contents = document.querySelectorAll('.content');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    contents.forEach(content => content.style.display = 'none');
+    contents[index].style.display = 'block';
+    buttons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
